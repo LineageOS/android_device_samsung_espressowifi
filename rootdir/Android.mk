d52281c52a0336a12cab3214b03fd17540608a53
Ziyan <jaraidaniel@gmail.com>
2015-08-17 17:21:00 +0200

    espresso-common: change fstab handling (1/3)
    Previously, fstab mounting was done in init.device.rc.
    This caused some problems, as for some reason, init.device.rc
    was executed after init.tab2.rc, not before, delaying the fstab mounts.
    
    Let's symlink fstab.espresso to fstab.espresso10 for espresso10 variants
    instead, so we don't need to use init.device.rc to mount fstab entries
    anymore.
    
    Change-Id: Ia8d2f292326cea90ac7aa19fa1ffce4fb13529d1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,16 +3,34 @@ include $(CLEAR_VARS)
 
 # boot.img parts
 include $(CLEAR_VARS)
-ifneq ($(filter p3100 p3110,$(TARGET_DEVICE)),)
 LOCAL_MODULE       := fstab.espresso
-else
-LOCAL_MODULE       := fstab.espresso10
-endif
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.tab2
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+ifneq ($(filter p5100 p5110,$(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := fstab.espresso10
+LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): TARGET := $(TARGET_ROOT_OUT)/fstab.espresso
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_ROOT_OUT)/$(LOCAL_MODULE)
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(TARGET) $(SYMLINK)
+	$(hide) touch $@
+endif
 
 include $(CLEAR_VARS)
 ifneq ($(filter p3100 p3110,$(TARGET_DEVICE)),)
@@ -47,22 +65,6 @@ endif
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.tab2.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-ifneq ($(filter p3100 p3110,$(TARGET_DEVICE)),)
-LOCAL_MODULE       := init.device.espresso.rc
-else
-LOCAL_MODULE       := init.device.espresso10.rc
-endif
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-ifneq ($(filter p3100 p3110,$(TARGET_DEVICE)),)
-LOCAL_SRC_FILES    := etc/init.device.espresso.rc
-else
-LOCAL_SRC_FILES    := etc/init.device.espresso10.rc
-endif
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
