bc343b41338403794b0fff784ec29f3bed1c9c6e
Ziyan <jaraidaniel@gmail.com>
2015-08-22 12:26:32 +0200

    espresso-common: audio: add parameters to open stream functions
    Bug: 14815883.
    Change-Id: Ie1aa70efa9e3a657379b56f9d3cb5ab56be13b46

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -2445,7 +2445,8 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
                                    audio_devices_t devices __unused,
                                    audio_output_flags_t flags __unused,
                                    struct audio_config *config,
-                                   struct audio_stream_out **stream_out)
+                                   struct audio_stream_out **stream_out,
+                                   const char *address __unused)
 {
     struct espresso_audio_device *ladev = (struct espresso_audio_device *)dev;
     struct espresso_stream_out *out;
@@ -2689,7 +2690,10 @@ static int adev_open_input_stream(struct audio_hw_device *dev,
                                   audio_io_handle_t handle __unused,
                                   audio_devices_t devices,
                                   struct audio_config *config,
-                                  struct audio_stream_in **stream_in)
+                                  struct audio_stream_in **stream_in,
+                                  audio_input_flags_t flags __unused,
+                                  const char *address __unused,
+                                  audio_source_t source __unused)
 {
     struct espresso_audio_device *ladev = (struct espresso_audio_device *)dev;
     struct espresso_stream_in *in;
