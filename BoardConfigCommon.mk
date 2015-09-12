89016499d2125bb9bb57c002e7648f117f96fda9
Ziyan <jaraidaniel@gmail.com>
2015-09-12 13:31:06 +0200

    espresso-common: commonize bluetooth configuration (1/3)
    Change-Id: I588357d320bc123134ec6bb054d5215b0a296587

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,6 +89,8 @@ BOARD_NO_APSME_ATTR              := true
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/espresso-common/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/espresso-common/bluetooth/vnd_espresso.txt
 
 # Charger
 BOARD_CHARGER_SHOW_PERCENTAGE := true
