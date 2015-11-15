b3b97a49a64e4caa7e318a1142f03351581b29f7
Kyle Repinski <repinski23@gmail.com>
2015-11-15 13:05:36 -0600

    Include common omap4 vendor repo if available.
    Note that TARGET_BOARD_OMAP_CPU needs to be set first.
    
    espresso:
    - original commit applied on omap4 hardware repo,
      need to be tested by motorola and amazon omap4 devices
      first - inherit from device tree until that
    
    Change-Id: I638f7af527b3724098d7379b58e30c7550aa1f16

--- device-common.mk
+++ device-common.mk
@@ -148,5 +148,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog
 
 $(call inherit-product-if-exists, vendor/samsung/espresso-common/espresso-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/ti/omap4/omap4-vendor.mk)
 $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
