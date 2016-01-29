dc4e4a66ee08c753a042149e9728dfc20917d1ac
Ziyan <jaraidaniel@gmail.com>
2016-01-29 12:12:51 +0100

    Move lcd density prop to espresso-common (1/5)
    Change-Id: I34b26213dd7ce219a52e57c1c3b1ac5a7d13fd03

--- espresso-common.mk
+++ espresso-common.mk
@@ -131,7 +131,8 @@ PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072 \
-    ro.bq.gpu_to_cpu_unsupported=1
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    ro.sf.lcd_density=160
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog
