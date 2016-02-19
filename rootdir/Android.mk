02d09c09fab12ab745ccfead828ba24b45275ea1
CCD-1997 <dcunha.cyprien@gmail.com>
2016-02-19 23:07:51 +0530

    rootdir: remove missed espresso/espresso10 distinction
    init.espresso.rc call init.espresso.usb.rc which is absent
    And hence usb connections doesn't work properly
    
    Change-Id: I93a4daf36692d9c982c92fe0a63a71b16e7eef71

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,11 +19,7 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-ifneq ($(filter p3100 p3110,$(TARGET_DEVICE)),)
 LOCAL_MODULE       := init.espresso.usb.rc
-else
-LOCAL_MODULE       := init.espresso10.usb.rc
-endif
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.tab2.usb.rc
