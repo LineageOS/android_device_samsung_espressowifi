Unlegacy-Android/android_device_samsung_espresso

Set up unified build for wifi-only variants

AndroidProducts.mk
@@ -1,0 +1,18 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_espresso.mk

vendorsetup.sh
@@ -1,0 +1,1 @@
+add_lunch_combo aosp_espresso-userdebug

LineageOS/android_device_samsung_espressowifi

Change-Id: I9fffe523aefac96e2cd0461b9398806b02e4c131

AndroidProducts.mk
@@ -1,0 +1,19 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_espresso.mk \
+    $(LOCAL_DIR)/cm.mk

vendorsetup.sh
@@ -1,0 +1,1 @@
+add_lunch_combo cm_espresso-userdebug