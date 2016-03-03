6bab92fbdd0a8547dd5c231f20eeb961fb8dcc9c
Kyle Repinski <repinski23@gmail.com>
2016-03-03 15:56:57 -0600

    libEGL_POWERVR: Improve eglChooseConfig+recordable workaround code.
    Change-Id: Ica4f022ebfd484d31d7848b68462e752e60ec520

--- libEGL_POWERVR/egl.h
+++ libEGL_POWERVR/egl.h
@@ -1,24 +1,14 @@
 #ifndef __PVREGL_H__
 #define __PVREGL_H__
 
+#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <cutils/compiler.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-
-#define EGL_FALSE	0
-#define EGL_TRUE	1
-
-#define EGL_SURFACE_TYPE	0x3033
-#define EGL_NONE	0x3038
-#define EGL_RENDERABLE_TYPE	0x3040
-#define EGL_RECORDABLE_ANDROID	0x3142
-
-#define EGL_PBUFFER_BIT	0x0001
-#define EGL_OPENGL_ES2_BIT	0x0004
 
 typedef int32_t EGLint;
 typedef unsigned int EGLBoolean;
@@ -31,6 +21,20 @@ typedef void *EGLClientBuffer;
 typedef int EGLNativeDisplayType;
 typedef void *EGLNativeWindowType;
 typedef void *EGLNativePixmapType;
+
+
+#define EGL_FALSE	0
+#define EGL_TRUE	1
+#define EGL_DONT_CARE	((EGLint)-1)
+
+#define EGL_SURFACE_TYPE	0x3033
+#define EGL_NONE	0x3038
+#define EGL_RENDERABLE_TYPE	0x3040
+#define EGL_RECORDABLE_ANDROID	0x3142
+
+#define EGL_PBUFFER_BIT	0x0001
+#define EGL_OPENGL_ES2_BIT	0x0004
+
 
 EGLint IMGeglGetError(void);
 EGLDisplay IMGeglGetDisplay(EGLNativeDisplayType display_id);
