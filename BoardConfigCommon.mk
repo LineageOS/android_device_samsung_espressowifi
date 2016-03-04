f99b70cd76ed7a52bc51e4a3e053cb92b1e1cc62
Ziyan <jaraidaniel@gmail.com>
2016-03-04 12:24:37 +0100

    sepolicy: address current denials
    Change-Id: Ied12c2b588856e7cb874e8693da7e07d9b8d0e6c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,7 +94,7 @@ BOARD_SEPOLICY_DIRS += \
     device/samsung/espressowifi/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    bluetooth.te \
+    cpboot-daemon.te \
     device.te \
     dock_kbd_attach.te \
     domain.te \
@@ -103,12 +103,13 @@ BOARD_SEPOLICY_UNION += \
     geomagneticd.te \
     gpsd.te \
     init.te \
+    mediaserver.te \
     orientationd.te \
     pvrsrvinit.te \
     smc_pa.te \
-    sysinit.te \
+    system_server.te \
     variant_setup.te \
-    wpa_supplicant.te
+    vold.te
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
