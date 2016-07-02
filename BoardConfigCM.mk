43afc97890ab12f21fa89618a906609df415203a
Andreas Blaesius <skate4life@gmx.de>
2016-07-02 14:04:29 +0200

    More cleanup, reorder files and sync with AOSP
    - move CM specific defines to a seperate BoardConfigCM.mk
    
    Change-Id: I1b00e4bef03f4553cc62dc1f5f25a417ffb00866

---
+++ BoardConfigCM.mk
@@ -0,0 +1,12 @@
+# Charger
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# F2FS filesystem
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Liblights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Recovery
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
+BOARD_HAS_DOWNLOAD_MODE := true
