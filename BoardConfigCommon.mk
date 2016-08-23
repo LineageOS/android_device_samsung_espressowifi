68c95398c090ce1e0cc9fe18edb26c46af18581f
Andreas Blaesius <skate4life@gmx.de>
2016-08-23 21:42:25 +0200

    espresso: set TARGET_BOOTANIMATION_TEXTURE_CACHE to true
    - fixes "slow motion" bootanimation:
      TARGET_BOOTANIMATION_TEXTURE_CACHE is set to false in omap4 hardware repo
      which slows down the bootanimation on Galaxy Tab 2
    
    - remove TARGET_BOOTANIMATION_USE_RGB565 because it is defined in
      omap4 hardware repo
    
    - inherit BoardConfigCM.mk after common omap4 BoardConfigCommon.mk
    
    Change-Id: I62bdfcd15e74975273b879617629a869e04c21d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,11 +15,11 @@
 # limitations under the License.
 #
 
-# Inherit CM specific board config
--include device/samsung/espressowifi/BoardConfigCM.mk
-
 # Inherit common omap4 board config
 -include hardware/ti/omap4/BoardConfigCommon.mk
+
+# Inherit CM specific board config
+-include device/samsung/espressowifi/BoardConfigCM.mk
 
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/espressowifi/include
 
