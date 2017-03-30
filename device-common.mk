3ea1f1c7fabc9d7043637f1df3250fc0c48bcfdc
Andreas Blaesius <skate4life@gmx.de>
2017-03-30 20:05:54 +0200

    espresso: remove android.hardware.audio.low_latency.xml
    - workaround to audio stuttering in some games on Android 5.x and newer
    
    Change-Id: Ifa7e2aa29e8656f37ae5dc18775ebfc699b1108b

--- device-common.mk
+++ device-common.mk
@@ -114,7 +114,6 @@ PRODUCT_PACKAGES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
