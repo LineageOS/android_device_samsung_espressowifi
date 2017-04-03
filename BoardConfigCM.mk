b5c8100808a3228b1dbb4ab4325c2ba2df7a63bf
Andreas Blaesius <skate4life@gmx.de>
2017-04-03 18:00:14 +0200

    espresso: Use LZMA compression for recovery's ramdisk again
    Fixes:
       error: +/ssd2/out/cm-14.1/target/product/espressowifi/recovery.img too large (9177088 > [8650752 - 88704]
    
    Change-Id: I24b2dee38d0f321584f99b4c53396b62a160c664

--- BoardConfigCM.mk
+++ BoardConfigCM.mk
@@ -10,6 +10,7 @@ TARGET_PROVIDES_LIBLIGHT := true
 # Recovery
 TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espressowifi
 BOARD_HAS_DOWNLOAD_MODE := true
+LZMA_RAMDISK_TARGETS := recovery
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
